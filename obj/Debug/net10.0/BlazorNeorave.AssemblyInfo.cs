7cc3a487f2daf67c8f1501d71062a27211a7c21e
Anderson M <evidenciasponao@gmail.com>
2026-06-05 00:03:32 +0200

    Fix: gitignore

--- obj/Debug/net10.0/BlazorNeorave.AssemblyInfo.cs
+++ obj/Debug/net10.0/BlazorNeorave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorNeorave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9615124342cf0b90087316f6532d76541b478b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorNeorave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorNeorave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
